02b9f04d22e7a4ce46417d15872fbb598eab9d90
Teemu Miettunen <miettune@linux-desktop10.tuni.fi>
2019-11-06 10:22:22 +0200

    Created class for worldmap

---
+++ Game/core/worldmap.cpp
@@ -0,0 +1,6 @@
+#include "worldmap.h"
+
+worldmap::worldmap()
+{
+
+}
---
+++ Game/core/worldmap.h
@@ -0,0 +1,11 @@
+#ifndef WORLDMAP_H
+#define WORLDMAP_H
+
+
+class worldmap
+{
+public:
+    worldmap();
+};
+
+#endif // WORLDMAP_H
